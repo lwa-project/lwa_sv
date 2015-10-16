23fa87d3eb16d770a8bff711baef36eacb0f1eb1
Ben Barsdell <benbarsdell@gmail.com>
2015-10-16 15:30:26 -0400

    Added basic Valon synthesizer scripts

---
+++ scripts/valon_program_204.8MHz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import valon_synth as valon
+
+if __name__ == "__main__":
+	import sys
+	device = "/dev/ttyUSB0"
+	if len(sys.argv) > 1:
+		device = sys.argv[1]
+	synth = valon.Synthesizer(device)
+	synth.set_label(valon.SYNTH_A, "Sampling clock") # Note: 16 char limit
+	synth.set_label(valon.SYNTH_B, "Tone injection") # Note: 16 char limit
+	print "Old synth A freq:", synth.get_frequency(valon.SYNTH_A)
+	synth.set_frequency(valon.SYNTH_A, 204.8, 0.8)
+	print "New synth A freq:", synth.get_frequency(valon.SYNTH_A)
+	synth.flash()
+
---
+++ scripts/valon_status.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import valon_synth as valon
+
+def print_synth(synth, which):
+	print "Synth A" if which == valon.SYNTH_A else "Synth B"
+	print "  Label:       ", synth.get_label(     which)
+	print "  Freq:        ", synth.get_frequency( which), "MHz"
+	print "  RF level:    ", synth.get_rf_level(  which), "dBm"
+	print "  Phase locked:", synth.get_phase_lock(which)
+	print "  VCO range:   ", synth.get_vco_range( which), "MHz"
+	options = synth.get_options(which)
+	doubled = options[0]
+	halved  = options[1]
+	print "  Scale:       ", ("unity"   if doubled==halved else
+	                          "doubled" if doubled else
+	                          "halved")
+	print "  Divisor:     ", options[2]
+	print "  Mode:        ", ("minimize PLL spurs" if options[3] else
+	                          "minimize phase noise")
+
+if __name__ == "__main__":
+	import sys
+	device = "/dev/ttyUSB0"
+	if len(sys.argv) > 1:
+		device = sys.argv[1]
+	synth = valon.Synthesizer(device)
+	ref_names = {valon.INT_REF: "internal",
+	             valon.EXT_REF: "external"}
+	print "Ref source:", ref_names[synth.get_ref_select()]
+	print "Ref freq:  ", synth.get_reference(), "MHz"
+	print_synth(synth, valon.SYNTH_A)
+	print_synth(synth, valon.SYNTH_B)
