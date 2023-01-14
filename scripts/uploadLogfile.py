8254ec14e13ba96253dbccfa7f04b8f7af582461
jaycedowell <jayce.dowell@gmail.com>
2023-01-14 07:34:56 -0700

    Use the new metadata end point.

--- scripts/uploadLogfile.py
+++ scripts/uploadLogfile.py
@@ -4,20 +4,20 @@
 import os
 import sys
 import time
-import requests
 from socket import gethostname
 
+from lwa_auth import KEYS as LWA_AUTH_KEYS
+from lwa_auth.signed_requests import post as signed_post
 
-URL = "https://lda10g.unm.edu/metadata/sorter/index.py"
-KEY = "c0843461abe746a4608dd9c897f9b261"
+URL = "https://lda10g.unm.edu/metadata/sorter/upload"
 SITE = "lwasv"
 TYPE = "SSLOG"
 
 # Send the update to lwalab
 r = os.path.realpath(sys.argv[1])
-f = requests.post(URL,
-                  data={'key': KEY, 'site': SITE, 'type': TYPE, 'subsystem': 'ADP'},
-                  files={'file': open(r, 'rb')},
-                  verify=False) # We don't have a certiticate for lda10g.unm.edu
+f = signed_post(LWA_AUTH_KEYS.get('adp', kind='private'), URL,
+                data={'site': SITE, 'type': TYPE, 'subsystem': 'ADP'},
+                files={'file': open(r, 'rb')},
+                verify=False) # We don't have a certiticate for lda10g.unm.edu
 print(f.text)
 f.close()
