82f8ed3916f6b0b554702596ddc4488392e5068f
jaycedowell <>
2023-08-22 09:05:08 -0600

    Back to alliance.

--- scripts/uploadLogfile.py
+++ scripts/uploadLogfile.py
@@ -9,7 +9,7 @@ from socket import gethostname
 from lwa_auth import KEYS as LWA_AUTH_KEYS
 from lwa_auth.signed_requests import post as signed_post
 
-URL = "https://lda10g.unm.edu/metadata/sorter/upload"
+URL = "https://lda10g.alliance.unm.edu/metadata/sorter/upload"
 SITE = "lwasv"
 TYPE = "SSLOG"
 
@@ -17,7 +17,6 @@ TYPE = "SSLOG"
 r = os.path.realpath(sys.argv[1])
 f = signed_post(LWA_AUTH_KEYS.get('adp', kind='private'), URL,
                 data={'site': SITE, 'type': TYPE, 'subsystem': 'ADP'},
-                files={'file': open(r, 'rb')},
-                verify=False) # We don't have a certiticate for lda10g.unm.edu
+                files={'file': open(r, 'rb')})
 print(f.text)
 f.close()
